5350a0d6f207d8acb3975515e3f6c83feb4a8e81
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-01-29 14:53:02 +0900

    beta

---
+++ Admin/FormAdminMenu.vb
@@ -0,0 +1,30 @@
+﻿Public Class FormAdminMenu
+    Private Sub btnAddLogin_Click(sender As Object, e As EventArgs) Handles btnAddStudent.Click
+        Me.ActiveControl = Nothing
+        FormAddStudent.Show()
+    End Sub
+
+    Private Sub btnModifyGrades_Click(sender As Object, e As EventArgs) Handles btnModifyGrades.Click
+        Me.ActiveControl = Nothing
+        FormModifyGrades.Show()
+    End Sub
+
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+        Me.ActiveControl = Nothing
+        FormLogin.Show()
+        FormLogin.txtboxLoginID.Clear()
+        FormLogin.txtboxPassword.Clear()
+        FormLogin.cmbUserType.SelectedIndex = -1
+        Me.Close()
+    End Sub
+
+    Private Sub btnAddAdmin_Click(sender As Object, e As EventArgs) Handles btnAddAdmin.Click
+        Me.ActiveControl = Nothing
+        FormAddAdmin.Show()
+    End Sub
+
+    Private Sub btnViewGrades_Click(sender As Object, e As EventArgs) Handles btnViewGrades.Click
+        Me.ActiveControl = Nothing
+        FormViewStudentGrades.Show()
+    End Sub
+End Class
---
+++ Main/Connection.vb
@@ -0,0 +1,3 @@
+﻿Public Class Connection
+    Public connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jamez\source\repos\Eclair-Student-Portal-AppMain\Eclair Student Portal.accdb"
+End Class
